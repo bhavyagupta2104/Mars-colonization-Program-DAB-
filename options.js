a51576d2cf6b84ffa5ec7549380858e4dd8036d8
ashima0699 <56577619+ashima0699@users.noreply.github.com>
2020-07-23 13:56:43 +0530

    Final Commit

--- options.js
+++ options.js
@@ -1,4 +1,3 @@
-
 const options = document.querySelector(".options");
 const gameOverElement = document.querySelector(".gameover");
 
@@ -15,72 +14,80 @@ let OPPONENT;
 let LEVEL;
 const player = new Object;
 
-computerBtn.addEventListener("click",function(){
-OPPONENT="computer";
+computerBtn.addEventListener("click", function () {
+  OPPONENT = "computer";
 
-switchActive(friendBtn,computerBtn);
-
-});
-friendBtn.addEventListener("click",function(){
-OPPONENT="friend";
-
-switchActive(computerBtn,friendBtn);
+  switchActive(friendBtn, computerBtn);
 
 });
-xBtn.addEventListener("click",function(){
-player.man="X";
-player.computer="O";
-player.friend="O";
+friendBtn.addEventListener("click", function () {
+  OPPONENT = "friend";
 
-switchActive(oBtn,xBtn);
+  switchActive(computerBtn, friendBtn);
 
 });
-oBtn.addEventListener("click",function(){
+xBtn.addEventListener("click", function () {
+  player.man = "X";
+  player.computer = "O";
+  player.friend = "O";
 
- player.man="O";
-player.computer="X";
-player.friend="X";
-
-switchActive(xBtn,oBtn);
-
-});
-easyBtn.addEventListener("click",function(){
-
-    LEVEL="easy";
-   
-   switchActive(diffBtn,easyBtn);
-   
-   });
-diffBtn.addEventListener("click",function(){
-
-   LEVEL="diff";
-   
-   switchActive(easyBtn,diffBtn);
-   
-   });
-playBtn.addEventListener("click",function(){
-
-    if(!OPPONENT){
-        computerBtn.style.backgroundColor = "#f00";
-        friendBtn.style.backgroundColor = "#f00";
-
-        return;
-    }
-
-    if(!player.man){
-        xBtn.style.backgroundColor = "#f00";
-        oBtn.style.backgroundColor = "#f00";
-
-        return;
-    }
-
-    init(player,OPPONENT);
-    options.classList.add("hide");
+  switchActive(oBtn, xBtn);
 
 });
+oBtn.addEventListener("click", function () {
 
-function switchActive(off,on)
-{
-    off.classList.remove("active");
-    on.classList.add("active");
+  player.man = "O";
+  player.computer = "X";
+  player.friend = "X";
+
+  switchActive(xBtn, oBtn);
+
+});
+easyBtn.addEventListener("click", function () {
+
+  LEVEL = "easy";
+
+  switchActive(diffBtn, easyBtn);
+
+});
+diffBtn.addEventListener("click", function () {
+
+  LEVEL = "diff";
+
+  switchActive(easyBtn, diffBtn);
+
+});
+playBtn.addEventListener("click", function () {
+
+  if (!OPPONENT) {
+    computerBtn.style.backgroundColor = "#ff471a";
+    friendBtn.style.backgroundColor = "#ff471a";
+    alert("Select either computer or friend");
+    return;
+  }
+
+  if (!player.man) {
+    xBtn.style.backgroundColor = "#ff471a";
+    oBtn.style.backgroundColor = "#ff471a";
+    
+    alert("Select a symbol");
+    return;
+  }
+  if (!LEVEL) {
+    easyBtn.style.backgroundColor = "#ff471a";
+    diffBtn.style.backgroundColor = "#ff471a";
+
+    alert("Select a Level");
+    return;
+  }
+
+
+  init(player, OPPONENT,LEVEL);
+  options.classList.add("hide");
+
+});
+
+function switchActive(off, on) {
+  off.classList.remove("active");
+  on.classList.add("active");
 }
